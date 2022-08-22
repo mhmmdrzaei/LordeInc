460cfd8672f75b2f28f053b3570aaa51fcae2b2d
Mohammad <mhmmdrzaei@gmail.com>
2022-08-22 12:17:52 -0400

    commercial Casting page

---
+++ page-commercial.php
@@ -0,0 +1,45 @@
+<?php //index.php is the last resort template, if no other templates match ?>
+<?php get_header(); ?>
+
+<main class="">
+	<section class="commericalSide">
+		<h1>
+	      <a href="<?php echo home_url( '/' ); ?>" title="<?php bloginfo( 'name', 'display' ); ?>" rel="home">
+	        <?php bloginfo( 'name' ); ?>
+	      </a>
+	    </h1>
+	    <h2><?php the_title(); ?></h2>
+
+		<section class="commercialItems">
+			<?php $args = array( 
+				'post_type' => 'commercial', 
+				'order' => 'ASC',
+				'posts_per_page' => -1
+				 );
+			  query_posts( $args ); // hijack the main loop
+			  while ( have_posts() ) : the_post();
+			    ?>
+			    <section class="commercialItemEach">
+			    	<a href="<?php the_permalink(); ?>">
+
+			    					<?php the_title(); ?>
+
+
+			    	</a>
+			    	<figure class="hide"><?php the_post_thumbnail('large'); ?></figure>
+			    </section>
+
+			    
+			   <?php
+			  endwhile;
+			  ?>
+			  <?php
+			  wp_reset_query();
+			  ?> 
+		</section>
+
+
+	</section>
+		
+</main>
+<?php get_footer(); ?>
---
+++ single-commercial.php
@@ -0,0 +1,77 @@
+<?php get_header(); ?>
+<main>
+  <section class="commercialMenu">
+      <h1>
+          <a href="<?php echo home_url( '/' ); ?>" title="<?php bloginfo( 'name', 'display' ); ?>" rel="home">
+            <?php bloginfo( 'name' ); ?>
+          </a>
+        </h1>
+    <a href="/commercial">&#x2190; Commercial Casting</a>
+
+  </section>
+  <?php if ( have_posts() ) while ( have_posts() ) : the_post(); ?>
+  <section class="castingPageContent">
+
+    <section class="castingPageInfo">
+      <h2 class="entry-title"><?php the_title(); ?></h2>
+      <?php the_field('casting_description'); ?>
+    </section>
+        <?php 
+        $video = get_field('casting_embedded_content');
+
+        if( !empty( $video ) ): ?>
+          <section class="embeddedVideo">
+            <section class="videoContainer"   aria-label="Embedded content container">
+               <?php the_field('casting_embedded_content'); ?>
+            </section>
+          </section>
+
+        <?php endif; ?>
+
+        <?php 
+        $videoFile = get_field('short_video');
+
+        if( !empty( $videoFile ) ): ?>
+          <section class="videoFile">
+            <section class="videoContainer"   aria-label="Embedded content container">
+               <video class="vid" muted controls controlsList="nodownload nofullscreen noremoteplayback">
+                 <source src="<?php the_field('short_video'); ?>" type="video/mp4">
+                 Your browser does not support the video tag.
+               </video>  
+            </section>
+          </section>
+
+
+        <?php endif; ?>
+
+        <?php if( have_rows('casting_images' ) ): ?>
+            <?php while( have_rows('casting_images') ): the_row(); ?>
+                    <?php
+                    $horizontalimage = get_sub_field('casting_image_commercial');
+                    if( $horizontalimage ):
+
+                        // Image variables.
+                        $url = $horizontalimage['url'];
+                    
+                        $alt = $horizontalimage['alt']; ?>
+                        <?php 
+                            if( get_sub_field('image_width') == 'Full Width' ): ?>
+                        <figure class="fullWidthHorizontal">
+                          <img src="<?php echo esc_url($url); ?>" alt="<?php echo esc_attr($alt); ?>">
+                        </figure>
+
+                    <?php elseif ( get_sub_field('image_width') == 'Half Width' ): ; ?>
+                      <figure class="halfWidthcasting">
+                        <img src="<?php echo esc_url($url); ?>" alt="<?php echo esc_attr($alt); ?>">
+                      </figure>
+                     <?php endif; ?>
+                   <?php endif; ?>
+
+          <?php endwhile; ?>
+        <?php endif; ?>
+  </section>
+  
+   <?php endwhile; // end of the loop. ?>
+</main>
+
+<?php get_footer(); ?>
